919b37e18acc3e20282ea0be5f84dd9f343a73fd
Marko  Buxar <39704542+MarkoBuxar@users.noreply.github.com>
2020-08-02 18:57:52 +0200

    Initial commit

---
+++ src/Config/Config.ts
@@ -0,0 +1,43 @@
+import config from './config.json';
+import path from 'path';
+import fs from 'fs-extra';
+import _ from 'lodash';
+import { Logger } from '../Logger/Logger';
+
+export class Config {
+  private static _instance: Config;
+
+  public static get Instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  public Get(p: string): any {
+    return _.get(config, p);
+  }
+
+  public Set(p: string, value: any): object {
+    _.set(config, p, value);
+    this.Save();
+    return config;
+  }
+
+  public Has(p: string): boolean {
+    return _.has(config, p);
+  }
+
+  public Remove(p: string) {
+    _.unset(config, p);
+    this.Save();
+  }
+
+  private Save() {
+    try {
+      fs.writeFileSync(
+        path.join(__dirname, 'config.json'),
+        JSON.stringify(config, null, 2),
+      );
+    } catch (exp) {
+      throw Logger.Error('Error saving to config');
+    }
+  }
+}
