1ed552028490879dace23886b7d5b0cf7f55f395
MarkoBuxar <buxarmarko@gmail.com>
2020-08-02 21:55:27 +0200

    implemented swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 
+import swagger from 'swagger-ui-express';
+import path from 'path';
+import YAML from 'yamljs';
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(
+  '/docs',
+  swagger.serve,
+  swagger.setup(YAML.load(path.join(__dirname, './swagger.yaml'))),
+);
 
 app.get('/', (req, res) => {
-  res.send('fuck');
+  res.send({});
 });
 
 app.listen(port, () => {
-  console.log('server started');
+  console.log(`Server started on port ${port}`);
 });
